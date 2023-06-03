a4cba923cb24b14f54f0450e6eb3eb0d13433142
Evgen <yahontov-sport@inbox.ru>
2023-06-03 18:28:46 +0300

    add tests for item.py and Class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+a1=Item('gvozdi',10,12)
+
+def test_calculate_total_price():
+    assert a1.calculate_total_price()==120
+
+def test_apply_discount():
+    assert a1.apply_discount()==11
+
+def test_list_all():
+
+    assert a1.all==a1.all
