68efc0f15deb4b420c41b1fc8372fa1fc5ba3ea2
Evgen <yahontov-sport@inbox.ru>
2023-06-08 15:39:00 +0300

    add tests for new def item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -12,3 +12,22 @@ def test_apply_discount():
 def test_list_all():
 
     assert a1.all==a1.all
+
+def test_name():
+    a1.name = 'Смартфон'
+    assert a1.name == 'Смартфон'
+
+def test_name_assert():
+    a1.name = 'Супертелефон'
+    assert a1.name=='gvozdi'
+    a1.name = 'Supergvozdi'
+    assert a1.name == 'gvozdi'
+
+def test_instantiate_from_csv():
+     Item.instantiate_from_csv()  # создание объектов из данных файла
+     assert len(Item.all) == 5
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
