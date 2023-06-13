7bf60460417e20c66760e898af43a501f3c49f42
Evgen <yahontov-sport@inbox.ru>
2023-06-13 17:09:41 +0300

    add def __add__ in item.py

--- src/item.py
+++ src/item.py
@@ -28,6 +28,8 @@ class Item:
 
     def __str__(self):
         return self.__name
+    def __add__(self, other):
+        return self.quantity+other.quantity
 
     def calculate_total_price(self) -> float:
         """
