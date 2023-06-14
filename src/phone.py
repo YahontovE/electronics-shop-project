ab1fe45e9f1a726b7fc7875ad959daa33d2fa9fe
Evgen <yahontov-sport@inbox.ru>
2023-06-14 14:48:20 +0300

    add @property and setter for number_of_sim in phone.py

--- src/phone.py
+++ src/phone.py
@@ -6,21 +6,21 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         if number_of_sim <=0:
             raise ValueError(': Количество физических SIM-карт должно быть целым числом больше нуля.')
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __add__(self, other):
         if not isinstance(other, Item):
             raise ValueError('Складывать можно только объекты Employee и дочерние от них.')
         return self.quantity + other.quantity
     def __repr__(self):
-        return f"{__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})" #"Phone('iPhone 14', 120000, 5, 2)"
+        return f"{__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
 
-m1=Item('Redmi',10,2)
-m2=Phone('OnePlas',100,12,1)
-#print(repr(m2))
-print(m2+m2)
-print(m1+m2)
-print(isinstance(m1,Item))
-#m2.number_of_sim = 0
-print(m2.number_of_sim)
+    @number_of_sim.setter
+    def number_of_sim(self,new):
+        if new <= 0 or not isinstance(new, int):
+            raise ValueError(f'Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.__number_of_sim=new
