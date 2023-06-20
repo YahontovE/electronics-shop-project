859d6e9acc9b87fedb202417b3e56764049a9b68
Evgen <yahontov-sport@inbox.ru>
2023-06-20 13:09:02 +0300

    add edits from PEP8

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -2,16 +2,20 @@ from src.keyboard import Keyboard
 import pytest
 
 kb = Keyboard('Dark Project KD87A', 9600, 5)
+
+
 def test__str__():
     assert str(kb) == "Dark Project KD87A"
     assert str(kb.language) == "EN"
 
+
 def test_change_lang():
     kb.change_lang()
     assert str(kb.language) == "RU"
-    #Сделали RU -> EN -> RU
+    # Сделали RU -> EN -> RU
     kb.change_lang().change_lang()
     assert str(kb.language) == "RU"
+
 
 def test_override_lang():
     with pytest.raises(AttributeError):
